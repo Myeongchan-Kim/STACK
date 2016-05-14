2a8ecf720aeb2e329a7790601c6d74ac8cdfcca5
Paul-Kim <kmc5500@naver.com>
2016-05-14 12:05:06 +0900

    add effectVariable to member

--- Stack/Renderer.h
+++ Stack/Renderer.h
@@ -30,15 +30,6 @@ private:
 	void CreateRenderState();
 	void CreateRenderState2();
 
-	struct MyVertex
-	{
-		XMFLOAT3	pos;
-		XMFLOAT4	color;
-
-		XMFLOAT3	normal;
-		XMFLOAT2	tex;
-	};
-
 	XMFLOAT4 lightDirection =
 	{
 		XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f)
@@ -53,33 +44,40 @@ private:
 	ID3D11Device*					m_device = nullptr;
 	ID3D11DeviceContext*			m_immediateContext = nullptr; //Dx¿ë DC
 	ID3D11RenderTargetView*			m_renderTargetView = nullptr;
-	D3D_FEATURE_LEVEL				m_featureLevel = D3D_FEATURE_LEVEL_11_0;
-	ID3D11Texture2D*				m_depthStencil = nullptr;
-	ID3D11DepthStencilView*			m_depthStencilView = nullptr;
+	D3D_FEATURE_LEVEL						m_featureLevel = D3D_FEATURE_LEVEL_11_0;
+	ID3D11Texture2D*						m_depthStencil = nullptr;
+	ID3D11DepthStencilView*					m_depthStencilView = nullptr;
 
-	ID3D11VertexShader*				m_vertexShader = nullptr;
-	ID3D11InputLayout*				m_inputLayout = nullptr;
-	ID3D11PixelShader*				m_pixelShader = nullptr;
-	ID3D11Buffer*					m_constantBuffer = nullptr;
-	ID3D11Buffer*					m_vertexBuffer = nullptr;
-	ID3D11Buffer*					m_indexBuffer = nullptr;
+	ID3D11VertexShader*						m_vertexShader = nullptr;
+	ID3D11InputLayout*						m_inputLayout = nullptr;
+	ID3D11PixelShader*						m_pixelShader = nullptr;
+	ID3D11Buffer*							m_constantBuffer = nullptr;
+	ID3D11Buffer*							m_vertexBuffer = nullptr;
+	ID3D11Buffer*							m_indexBuffer = nullptr;
 
-	ID3D11RasterizerState*			m_solidRS = nullptr;
-	ID3D11RasterizerState*			m_wireFrameRS = nullptr;
+	ID3D11RasterizerState*					m_solidRS = nullptr;
+	ID3D11RasterizerState*					m_wireFrameRS = nullptr;
 
-	ID3D11ShaderResourceView*		m_textureRV = nullptr;
-	ID3D11Resource*					m_texture = nullptr;
-	ID3D11SamplerState*				m_samplerLinear = nullptr;
-	ID3DX11Effect*					m_effect = nullptr;
-	ID3DX11EffectTechnique*			m_tech = nullptr;
+	ID3D11ShaderResourceView*				m_textureRV = nullptr;
+	ID3D11Resource*							m_texture = nullptr;
+	ID3D11SamplerState*						m_samplerLinear = nullptr;
+	ID3DX11Effect*							m_effect = nullptr;
+	ID3DX11EffectTechnique*					m_tech = nullptr;
+	ID3DX11EffectSamplerVariable*			m_samLinear = nullptr;
+	ID3DX11EffectShaderResourceVariable*	m_texDiffuse = nullptr;
+	ID3DX11EffectMatrixVariable*			m_wvp;
+	ID3DX11EffectMatrixVariable*			m_world;
+	ID3DX11EffectVectorVariable*			m_lightDir;
+	ID3DX11EffectVectorVariable*			m_lightColor;
 
-	XMMATRIX						m_world;
-	XMMATRIX						m_world2;
-	XMMATRIX						m_view;
-	XMMATRIX						m_projection;
 
-	int								m_width;
-	int								m_height;
+	//XMMATRIX								m_world;
+	XMMATRIX								m_world2;
+	XMMATRIX								m_view;
+	XMMATRIX								m_projection;
+
+	int										m_width;
+	int										m_height;
 
 
 	struct	 ConstantBuffer
