dd17a3fb21c17cc27fefeeaa44aa179d8dd65e20
Paul-Kim <kmc5500@naver.com>
2016-05-18 12:02:10 +0900

    add change direction mode

--- Stack/GameScene.h
+++ Stack/GameScene.h
@@ -13,8 +13,9 @@ private:
 	bool IsOn(ModelClass* b1, ModelClass* b2);
 	ModelClass* m_currentBlock = nullptr;
 	ModelClass* m_lastBlock = nullptr;
-	Vector3 m_curPos = { 0, 0, 0 };
-	bool m_isEnd = false;
+	Vector3 m_curPos = { 0.0f, 0.0f, 0.0f };
+	bool m_isEnd = false; //엔딩모드냐 아니냐
+	Vector3 m_curMoveDir = {0.0f, 0.0f, 0.0f};
 
 	ModelClass* m_backGround = nullptr;
 	XMFLOAT3 m_color;
