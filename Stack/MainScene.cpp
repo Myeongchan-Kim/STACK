8105ceec52a6486f51f10391ccc9e55f5e484627
Taewoo Kim <kimsin3003@naver.com>
2016-05-24 20:38:32 +0900

    Main Scene Ok.

--- Stack/MainScene.cpp
+++ Stack/MainScene.cpp
@@ -7,8 +7,10 @@
 void MainScene::Start(Camera& camera)
 {
 	camera.InitCamera();
+	float viewWidth = ConstVars::DEFAULT_VIEW_WIDTH;
+	float viewHeight = ConstVars::DEFAULT_VIEW_HEIGHT;
+	camera.SetProjection(viewWidth, viewHeight);
 	m_background = new ModelClass();
-	//m_backGround->SetToRectangle(camera.GetViewSizeWidth(), camera.GetViewSizeHeight(), { 0.0f, 1.0f, 0.0f });
 	m_background->SetPosition(0,0,0);
 	m_background->SetRGB(1, 1, 1);
 	m_background->SetToRectangle(camera.GetViewSizeWidth(), camera.GetViewSizeHeight(), { 0.0f, 0.0f, -1.0f });
