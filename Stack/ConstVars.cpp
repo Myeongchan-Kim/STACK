d64b29e7c5755029ed967c33e9e573a07b338084
Taewoo Kim <kimsin3003@naver.com>
2016-05-24 19:01:52 +0900

    release over.

--- Stack/ConstVars.cpp
+++ Stack/ConstVars.cpp
@@ -2,12 +2,12 @@
 
 	const float ConstVars::DEFAULT_VIEW_WIDTH = 10.0f;
 	const float ConstVars::DEFAULT_VIEW_HEIGHT = 10.0f;
-	WCHAR* ConstVars::CONCREAT_TEX_FILE = L"texture/concrete.jpg";
-	WCHAR* ConstVars::FEBRIC_TEX_FILE = L"texture/febric.jpg";
-	WCHAR* ConstVars::PLANE_TEX_FILE = L"texture/plane.jpg";
-	WCHAR* ConstVars::BLUE_TILE_TEX_FILE = L"texture/blue_tile.jpg";
-	WCHAR* ConstVars::COLORFUL_TILE_TEX_FILE = L"texture/colorful_tile.jpg";
+	WCHAR* ConstVars::CONCREAT_TEX_FILE = L"Texture/concrete.jpg";
+	WCHAR* ConstVars::FEBRIC_TEX_FILE = L"Texture/febric.jpg";
+	WCHAR* ConstVars::PLANE_TEX_FILE = L"Texture/plane.jpg";
+	WCHAR* ConstVars::BLUE_TILE_TEX_FILE = L"Texture/blue_tile.jpg";
+	WCHAR* ConstVars::COLORFUL_TILE_TEX_FILE = L"Texture/colorful_tile.jpg";
 		  
 	WCHAR* ConstVars::VISIBLE_TEX_FILE = ConstVars::CONCREAT_TEX_FILE;
 	WCHAR* ConstVars::VANISHING_TEX_FILE = ConstVars::CONCREAT_TEX_FILE;
-	WCHAR* ConstVars::MAIN_MENU_TEX_FILE = L"texture/start.png";
+	WCHAR* ConstVars::MAIN_MENU_TEX_FILE = L"Texture/start.png";
