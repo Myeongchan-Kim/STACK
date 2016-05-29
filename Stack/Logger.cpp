b874ed78e688f8a0c266d816e111e658208609f0
Paul-Kim <kmc5500@naver.com>
2016-05-30 00:35:23 +0900

    change logger to fstream

--- Stack/Logger.cpp
+++ Stack/Logger.cpp
@@ -2,10 +2,12 @@
 #include "ConstVars.h"
 #include <string>
 #include <ctime>
+#include <fstream>
 
 Logger::Logger()
 {
-	int rc = unqlite_open(&m_db, "data.db", UNQLITE_OPEN_CREATE);
+	//int rc = unqlite_open(&m_db, "data.db", UNQLITE_OPEN_CREATE);
+	
 }
 
 
@@ -15,7 +17,7 @@ Logger::~Logger()
 
 void Logger::ShutDown()
 {
-	unqlite_close(m_db);
+	//unqlite_close(m_db);
 }
 
 
@@ -23,33 +25,46 @@ void Logger::ShutDown()
 int Logger::LoadRecord()
 {
 
-	int rc;
-	unqlite_int64 nBytes;
-	//Extract data size first
-   	rc = unqlite_kv_fetch(m_db, "max", -1, NULL, &nBytes);
-	if (rc != UNQLITE_OK) { return 0; }
 
-	//Allocate a buffer big enough to hold the record content
-	char* zBuf = (char *)malloc(nBytes);
-	if (zBuf == NULL) { return 0; }
-	
-	
-	rc = unqlite_kv_fetch(m_db, "max", -1, zBuf, &nBytes);
-	while (rc == UNQLITE_BUSY)
-	{
-		Sleep(20);
-		rc = unqlite_kv_fetch(m_db, "max", -1, zBuf, &nBytes);
+	//int rc;
+	//unqlite_int64 nBytes;
+	////Extract data size first
+	// 	rc = unqlite_kv_fetch(m_db, "max", -1, NULL, &nBytes);
+	//if (rc != UNQLITE_OK) { return 0; }
+
+	////Allocate a buffer big enough to hold the record content
+	//char* zBuf = (char *)malloc(nBytes);
+	//if (zBuf == NULL) { return 0; }
+	//
+	//
+	//rc = unqlite_kv_fetch(m_db, "max", -1, zBuf, &nBytes);
+	//while (rc == UNQLITE_BUSY)
+	//{
+	//	Sleep(20);
+	//	rc = unqlite_kv_fetch(m_db, "max", -1, zBuf, &nBytes);
+	//}
+
+	//int result = std::stoi(std::string(zBuf));
+	//
+	//delete zBuf;
+
+	int result;
+	std::ifstream ifs("highScore.bin", std::ios::binary);
+
+	if (ifs.good()) {
+		ifs >> result;
 	}
-
-	int result = std::stoi(std::string(zBuf));
-	
-	delete zBuf;
+	else
+	{
+		result = 0;
+	}
 
 	return result;
 }
 
 void Logger::SaveRecord(int maxCount)
 {
+	/*
 	int rc;
 	std::string score = std::to_string(maxCount);
 	
@@ -58,6 +73,7 @@ void Logger::SaveRecord(int maxCount)
 		return;
 
 	int len = score.size();
+	
 	rc = unqlite_kv_store(m_db, "max", -1, score.c_str(), len);
 
 	while (rc == UNQLITE_BUSY)
@@ -65,7 +81,14 @@ void Logger::SaveRecord(int maxCount)
 		Sleep(20);
 		rc = unqlite_kv_store(m_db, "max", -1, score.c_str(), len);
 	}
+	*/
+	int oldRecord = LoadRecord();
 
+	if (maxCount > oldRecord)
+	{
+		std::ofstream ofs("highScore.bin", std::ios::binary);
+		ofs << maxCount;
+	}
 	return;
 }
 
@@ -80,18 +103,22 @@ void Logger::SavePlayLog(int Count, float Height)
 	localtime_s(timeinfo, &curTime);
 	char timeStr[20] = { 0, };
 	strftime(timeStr, 20, "%Y%m%d-%X", timeinfo); //20160617-14:55:02
-	std::string result = std::string(timeStr) + " " + score + " " + height +"\n"; //20160617-14:55:02 34 34.0\n
+	std::string result = std::string(timeStr) + " " + score + " " + height; //20160617-14:55:02 34 34.0\n
 	int len = result.size();
 	
-	rc = unqlite_kv_append(m_db, "PLAYLOG", -1, result.c_str(), len);;
-	while (rc == UNQLITE_BUSY)
-	{
-		Sleep(20);
-		rc = unqlite_kv_append(m_db, "PLAYLOG", -1, result.c_str(), len);;
-	}
+	//rc = unqlite_kv_append(m_db, "PLAYLOG", -1, result.c_str(), len);;
+	//while (rc == UNQLITE_BUSY)
+	//{
+	//	Sleep(20);
+	//	rc = unqlite_kv_append(m_db, "PLAYLOG", -1, result.c_str(), len);;
+	//}
 
-	unqlite_commit(m_db);
-	delete timeinfo;
+	//unqlite_commit(m_db);
+	//delete timeinfo;
+
+	
+	std::ofstream ofs("playLog.log", std::ios::app);
+	ofs << result << std::endl;
 
 	return;
 }
